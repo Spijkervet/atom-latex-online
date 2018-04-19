4f75fef2f62102b59b7f1877e1f59d512139802f
Janne Spijkervet <janne.spijkervet@hotmail.com>
2018-04-19 16:54:33 +0200

    Latex Online Atom

---
+++ spec/latex-online-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomPackageView from '../lib/latex-online-view';
+
+describe('AtomPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
